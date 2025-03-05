c36bfc69c6d67f29be64e8efe25af92ab027f456
Danim01 <danibuimon.09@gmail.com>
2025-03-04 23:59:34 -0500

    reset password process

---
+++ app/(login)/password/reset/verify.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+function VerifyPasswordScreen() {
+  return (
+    <ThemedView>
+      <ThemedText>Verificando...</ThemedText>
+    </ThemedView>
+  )
+}
+
+export default VerifyPasswordScreen
